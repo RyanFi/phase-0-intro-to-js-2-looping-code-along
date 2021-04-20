77f59f0b4e6a5cd20593fa60e26a0ae4f260afe4
RyanFi <ryanmcmahon94@gmail.com>
2021-04-19 20:52:23 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(nameArray, event) {
+  let cardsArray = []
+  for (let i = 0; i < nameArray.length; i++) {
+      cardsArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return cardsArray
+}
+
+function countDown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1
+  }
+  console.log(number)
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -29,7 +29,7 @@ describe( 'index.js', () => {
       expect( spy.callCount, "Expected countDown(10) to invoke 11 console.logs" )
         .to.eq( 11 );
     } );
-
+ 
     it( 'logs each number when counting down, starting from the number provided', () => {
       countDown( 4 );
       expect( spy.calledWithExactly( 4 ), "Expected countDown(4) to log 4 first" )
